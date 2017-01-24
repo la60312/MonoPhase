e7d9fd41cc1f472870cd94edd649590b5294f894
Tzung-Chien Hsieh <s8tzhsie@stud.uni-saarland.de>
2017-01-24 15:57:21 +0100

    Modify iteration run on scphaser
    Change output dir to cv_results/scphaser_ite

--- analysis_scphaser_cv.py
+++ analysis_scphaser_cv.py
@@ -4,10 +4,10 @@ import sys
 min_count = 3
 fc = 3
 min_sample = 5
-#os.system("~/R-3.3.2/bin/Rscript run_scphaser_cv.R")
+os.system("~/R-3.3.2/bin/Rscript run_scphaser_cv.R")
 
-for sample_count in range(6, 28, 2):
-	for ite in range(0, 5):
+for sample_count in range(6, 30, 2):
+	for ite in range(0, 10):
 	
 		os.system("python get_scphaser_vcf_cv.py " + str(sample_count) + " " + str(ite))
 		
--- scphaser_cv.py
+++ scphaser_cv.py
@@ -6,7 +6,7 @@ path = ""
 sample_count = sys.argv[1]
 ite = sys.argv[2]
 
-prefix = "tophat_scphaser_cv/scphaser_tophat_"
+prefix = "/MMCI/MS/RNAHap/work/single_cel/tophat_scphaser_cv/scphaser_tophat_"
 
 
 input_type = ['ac', 'gt']
@@ -18,12 +18,26 @@ method_index = 0
 type_index = 0
 weight_index = 1
 input_file = prefix + sample_count + "_" + ite + ".vcf"
-out_file = prefix + sample_count + "_" + ite + ".vcf"
+out_dir = '/MMCI/MS/RNAHap/work/single_cel/cv_results/scphaser_' + sample_count
 
-cmd = "whatshap compare --sample NA12878 " + input_file + " ../vcf_NA12878_giab/NA12878_chr_het_GTPS.vcf > " + out_file + ".compare"
+if not os.path.exists(out_dir):
+	os.system('mkdir ' + out_dir)
+os.chdir(out_dir)
+
+out_file = out_dir + "/scphaser_" + ite + ".vcf"
+cmd = "~/whatshap/venv/bin/whatshap compare --sample NA12878 " + input_file + " /MMCI/MS/RNAHap/work/vcf_NA12878_giab/NA12878_chr_het_GTPS.vcf > " + out_file + ".compare"
 os.system(cmd)
 
-ite_file = prefix + sample_count + "_" + ite + "_ite.csv" 
-os.system('python generate_csv.py ' + out_file + '.compare ' +  ite_file)
+# Move all chr file to method_
+cmp_dir = out_dir + '/compare_ite_' + ite
+
+if not os.path.exists(cmp_dir):
+	os.system('mkdir ' + cmp_dir)
+
+os.system("mv -f chr* " + cmp_dir)
+
+# Generate csv
+os.system('rm -f ' + out_file)
+os.system('python generate_csv.py ' + out_file + '.compare '  + out_dir + '/ite_' + ite + '.csv')
 
 
